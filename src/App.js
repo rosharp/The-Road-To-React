59be00b71cde60ab29fe1ca24b975c4bc7ec130a
Ро <ro.sharp@MacBook-Pro-Ro.local>
2022-03-08 22:01:54 +1000

    React Fragments

--- src/App.js
+++ src/App.js
@@ -70,15 +70,16 @@ const App = () => {
 //-----------------//
 
 const Search = ({ search, onSearch }) => (
-  <div>
-    <label htmlFor="search">Search: </label>
+  <>
+    <label key="1" htmlFor="search">Search: </label>
     <input
+      key="2"
       id="search"
       type="text"
       value={search}
       onChange={onSearch}
     />
-  </div>
+  </>
 );
 
 
